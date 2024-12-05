bc56ac06c5cf30c3638c986ba9c1011cc0603609
SjirkW <sjirkwittermans@gmail.com>
2024-12-05 20:27:32 +0100

    cleanup day 3

--- days/day3/day3.go
+++ days/day3/day3.go
@@ -4,11 +4,10 @@ import (
 	"aoc/utils"
 	"fmt"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
-func run(checkEnable bool) int {
+func run(doEnableCheck bool) int {
 	lines := utils.ReadInputAsLines(3, false)
 
 	total := 0
@@ -21,7 +20,7 @@ func run(checkEnable bool) int {
 			leftSide += string(rightSide[0])
 			rightSide = rightSide[1:]
 
-			if checkEnable {
+			if doEnableCheck {
 				if strings.HasSuffix(leftSide, "do()") {
 					enabled = true
 				} else if strings.HasSuffix(leftSide, "don't()") {
@@ -30,13 +29,12 @@ func run(checkEnable bool) int {
 			}
 
 			if enabled && strings.HasSuffix(leftSide, "mul") {
-				re := regexp.MustCompile(`\((\d+),(\d+)\)`)
+				re := regexp.MustCompile(`^\((\d+),(\d+)\)`)
 				matches := re.FindStringSubmatch(rightSide)
-				matchIsAtStart := re.FindStringSubmatchIndex(rightSide)[0] == 0
 
-				if matchIsAtStart && len(matches) > 2 {
-					x, _ := strconv.Atoi(matches[1])
-					y, _ := strconv.Atoi(matches[2])
+				if len(matches) > 2 {
+					x := utils.StringToInt(matches[1])
+					y := utils.StringToInt(matches[2])
 					total += x * y
 				}
 			}
--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"aoc/days/day5"
-)
+import "aoc/days/day3"
 
 func main() {
 	// day1.Solve()
 	// day2.Solve()
-	// day3.Solve()
+	day3.Solve()
 	// day4.Solve()
-	day5.Solve()
+	// day5.Solve()
 }
