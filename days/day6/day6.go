c8c634f6b517e8477797ca77e35e105e2f3bfe7f
SjirkW <sjirkwittermans@gmail.com>
2024-12-06 22:47:02 +0100

    clean

--- days/day6/day6.go
+++ days/day6/day6.go
@@ -13,6 +13,14 @@ var directions = [][]int{
 	{-1, 0},
 }
 
+type GridData struct {
+	grid      [][]string
+	position  []int
+	direction []int
+	score     int
+	obstacles map[string]int
+}
+
 func indexOfDirection(direction []int, directions [][]int) int {
 	for i, dir := range directions {
 		if dir[0] == direction[0] && dir[1] == direction[1] {
@@ -22,42 +30,48 @@ func indexOfDirection(direction []int, directions [][]int) int {
 	return -1
 }
 
-func traverseGrid(grid [][]string, x int, y int, direction []int, steps int) {
+func traverseGrid(gridData GridData) {
 	// utils.PrintGrid(grid)
+	x := gridData.position[0]
+	y := gridData.position[1]
+	grid := gridData.grid
+	score := gridData.score
+	direction := gridData.direction
 	previous := []int{x, y}
 
-	dx := direction[0]
-	dy := direction[1]
-	horizontalIx := x + dx
-	verticalIx := y + dy
+	horizontalIx := x + gridData.direction[0]
+	verticalIx := y + gridData.direction[1]
 	// Bounds check
 	if horizontalIx < 0 || horizontalIx >= len(grid) {
 		utils.PrintGrid(grid)
-		fmt.Print("Out of bounds", steps)
+		fmt.Print("Out of bounds", score)
 		return
 	}
 	if verticalIx < 0 || verticalIx >= len(grid[0]) {
 		utils.PrintGrid(grid)
-		fmt.Print("Out of bounds", steps)
+		fmt.Print("Out of bounds", score)
 		return
 	}
 
 	next := grid[verticalIx][horizontalIx]
 	if next == "." || next == "^" {
 		grid[verticalIx][horizontalIx] = "X"
-		traverseGrid(grid, horizontalIx, verticalIx, direction, steps+1)
+		gridData.position = []int{horizontalIx, verticalIx}
+		gridData.score++
+		traverseGrid(gridData)
 	} else if next == "X" {
-		traverseGrid(grid, horizontalIx, verticalIx, direction, steps)
+		gridData.position = []int{horizontalIx, verticalIx}
+		traverseGrid(gridData)
 	} else if next == "#" {
 		nextDirection := directions[(indexOfDirection(direction, directions)+1)%len(directions)]
-		traverseGrid(grid, previous[0], previous[1], nextDirection, steps)
+		gridData.direction = nextDirection
+		gridData.position = []int{previous[0], previous[1]}
+		traverseGrid(gridData)
 	}
-
-	// fmt.Print(grid[horizontalIx][verticalIx])
 }
 
 func Solve() {
-	lines := utils.ReadInputAsLines(6, false)
+	lines := utils.ReadInputAsLines(6, true)
 
 	var grid [][]string
 	for _, line := range lines {
@@ -74,5 +88,13 @@ func Solve() {
 		}
 	}
 
-	traverseGrid(grid, start[0], start[1], directions[0], 0)
+	gridData := GridData{
+		grid:      grid,
+		position:  start,
+		direction: directions[0],
+		score:     0,
+		obstacles: make(map[string]int),
+	}
+
+	traverseGrid(gridData)
 }
