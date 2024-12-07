813b9cf6680bdace9e85bfaa35ee1da88eb7a5bb
SjirkW <sjirkwittermans@gmail.com>
2024-12-07 17:30:28 +0100

    faster concat

--- days/day7/day7.go
+++ days/day7/day7.go
@@ -7,12 +7,20 @@ import (
 	"strings"
 )
 
-func concatInts(a, b int) int {
-	return a*int(math.Pow(10, math.Floor(math.Log10(float64(b))+1))) + b
+func concatInts(a int, b int) int {
+	if b < 10 {
+		return a*10 + b
+	} else if b < 100 {
+		return a*100 + b
+	} else if b < 1000 {
+		return a*1000 + b
+	} else {
+		return a*int(math.Pow(10, math.Floor(math.Log10(float64(b))+1))) + b
+	}
 }
 
 func hasResult(result int, current int, numbers []int, concat bool) bool {
-	if len(numbers) == 0 {
+	if len(numbers) == 0 || current > result {
 		return current == result
 	}
 
@@ -46,8 +54,7 @@ func Solve() {
 	for i := 0; i < len(left); i++ {
 		if hasResult(left[i], 0, right[i], false) {
 			pt1 += left[i]
-		}
-		if hasResult(left[i], 0, right[i], true) {
+		} else if hasResult(left[i], 0, right[i], true) {
 			pt2 += left[i]
 		}
 	}
