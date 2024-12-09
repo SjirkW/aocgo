4fbd373006a672a6e5813216845f0e246685ae63
SjirkW <sjirkwittermans@gmail.com>
2024-12-09 19:14:32 +0100

    almost 2

--- days/day9/day9.go
+++ days/day9/day9.go
@@ -5,8 +5,19 @@ import (
 	"fmt"
 )
 
+func getResult(blocks []int) int {
+	result := 0
+	for i, block := range blocks {
+		if block != -1 {
+			result += block * i
+		}
+	}
+
+	return result
+}
+
 func Solve() {
-	lines := utils.ReadInputAsLines(9, false)
+	lines := utils.ReadInputAsLines(9, true)
 	line := lines[0]
 
 	// fmt.Println(line)
@@ -15,6 +26,8 @@ func Solve() {
 	isBlock := true
 
 	var blocks []int
+	blockSizes := make(map[int]int)
+	emptySizes := make(map[int]int)
 	for _, char := range line {
 		count := utils.StringToInt(string(char))
 
@@ -22,11 +35,16 @@ func Solve() {
 			if isBlock {
 				blocks = append(blocks, idCounter)
 			} else {
+				if i == 0 {
+					emptySizes[len(blocks)] = count
+				}
 				blocks = append(blocks, -1)
 			}
 		}
 
-		if !isBlock {
+		if isBlock {
+			blockSizes[idCounter] = count
+		} else {
 			idCounter++
 		}
 		isBlock = !isBlock
@@ -42,6 +60,11 @@ func Solve() {
 			break
 		}
 	}
+
+	// copy blocks to blocks2
+	pt2Blocks := make([]int, len(blocks))
+	copy(pt2Blocks, blocks)
+
 	for i := len(blocks) - 1; i >= 0; i-- {
 		blocks[firstEmpty] = blocks[i]
 		blocks[i] = -1
@@ -59,12 +82,33 @@ func Solve() {
 		}
 	}
 
-	result := 0
-	for i, block := range blocks {
-		if block != -1 {
-			result += block * i
+	fmt.Println("Part 1:", getResult(blocks))
+
+	for i := len(pt2Blocks) - 1; i >= 0; i-- {
+		id := pt2Blocks[i]
+		length := blockSizes[id]
+
+		if id == -1 {
+			continue
+		}
+		for j := 0; j < len(pt2Blocks); j++ {
+			if (pt2Blocks[j] == -1) &&
+				(emptySizes[j] > 0 && emptySizes[j] >= length) {
+				for x := 0; x < length; x++ {
+					pt2Blocks[j+x] = id
+					pt2Blocks[i-x] = -1
+				}
+
+				oldSize := emptySizes[j]
+				delete(emptySizes, j)
+				emptySizes[j+length] = oldSize - length
+				break
+			}
 		}
 	}
 
-	fmt.Println(result)
+	fmt.Println("Part 2:", pt2Blocks)
+	fmt.Println("Part 2:", getResult(pt2Blocks))
+	// fmt.Println(blockSizes)
+	// fmt.Println(pt2Blocks)
 }
