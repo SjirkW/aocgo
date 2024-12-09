a2f69b756e0c3f07f3dc69ca70768fc5093a73ee
SjirkW <sjirkwittermans@gmail.com>
2024-12-09 18:36:16 +0100

    pt1

--- days/day9/day9.go
+++ days/day9/day9.go
@@ -6,22 +6,24 @@ import (
 )
 
 func Solve() {
-	lines := utils.ReadInputAsLines(9, true)
+	lines := utils.ReadInputAsLines(9, false)
 	line := lines[0]
 
-	fmt.Println(line)
+	// fmt.Println(line)
 
 	idCounter := 0
 	isBlock := true
 
-	var blocks [][]int
+	var blocks []int
 	for _, char := range line {
 		count := utils.StringToInt(string(char))
-		if isBlock {
-			blocks = append(blocks, make([]int, 2))
-			blocks[idCounter][0] = count
-		} else {
-			blocks[idCounter][1] = count
+
+		for i := 0; i < count; i++ {
+			if isBlock {
+				blocks = append(blocks, idCounter)
+			} else {
+				blocks = append(blocks, -1)
+			}
 		}
 
 		if !isBlock {
@@ -30,24 +32,39 @@ func Solve() {
 		isBlock = !isBlock
 	}
 
-	fmt.Println(blocks)
+	// fmt.Println(blocks)
 
 	// Loop backwards
-	firstEmptySpaceIndex := -1
-	for index, block := range blocks {
-		if block[1] > 0 {
-			firstEmptySpaceIndex = index
+	firstEmpty := -1
+	for i, _ := range blocks {
+		if blocks[i] == -1 {
+			firstEmpty = i
+			break
+		}
+	}
+	for i := len(blocks) - 1; i >= 0; i-- {
+		blocks[firstEmpty] = blocks[i]
+		blocks[i] = -1
+
+		// Find next empty
+		for x := firstEmpty; x < len(blocks); x++ {
+			if blocks[x] == -1 {
+				firstEmpty = x
+				break
+			}
+		}
+
+		if firstEmpty >= i {
 			break
 		}
 	}
 
-	fmt.Println("First empty space index: ", firstEmptySpaceIndex)
-
-	for i := len(blocks) - 1; i >= 0; i-- {
-		block := blocks[i]
-		blockAmount := block[0]
-		for j := 0; j < blockAmount; j++ {
-			blocks[firstEmptySpaceIndex][= block[1]
+	result := 0
+	for i, block := range blocks {
+		if block != -1 {
+			result += block * i
 		}
 	}
+
+	fmt.Println(result)
 }
