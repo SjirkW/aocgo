81e3085b0b446a546b9c26f3fb74fe4782cb3a12
SjirkW <sjirkwittermans@gmail.com>
2024-12-15 20:27:46 +0100

    pt1

--- days/day12/day12.go
+++ days/day12/day12.go
@@ -6,31 +6,39 @@ import (
 	"strings"
 )
 
-var fillMap = make(map[int]bool)
+type FloodFillResult struct {
+	Area      int
+	Perimeter int
+}
 
-func FloodFill(grid [][]string, x int, y int, fill string) int {
+func FloodFill(grid [][]string, x int, y int, fill string, result *FloodFillResult) {
 	if x < 0 || x >= len(grid) || y < 0 || y >= len(grid[0]) {
-		return 0
+		result.Perimeter++
+		return
 	}
 
-	if grid[y][x] != fill || grid[y][x] == strings.ToLower(fill) {
-		return 0
+	if grid[y][x] == strings.ToLower(fill) {
+		return
+	} else if grid[y][x] != fill {
+		result.Perimeter++
+		return
 	}
 
+	// Mark the cell as visited
 	grid[y][x] = strings.ToLower(fill)
 
-	result := 1
+	// Increment the count
+	result.Area++
 
-	result += FloodFill(grid, x+1, y, fill)
-	result += FloodFill(grid, x-1, y, fill)
-	result += FloodFill(grid, x, y+1, fill)
-	result += FloodFill(grid, x, y-1, fill)
-
-	return result
+	// Explore neighbors
+	FloodFill(grid, x+1, y, fill, result)
+	FloodFill(grid, x-1, y, fill, result)
+	FloodFill(grid, x, y+1, fill, result)
+	FloodFill(grid, x, y-1, fill, result)
 }
 
 func Solve() {
-	lines := utils.ReadInputAsLines(12, true)
+	lines := utils.ReadInputAsLines(12, false)
 
 	grid := make([][]string, len(lines))
 	for i, line := range lines {
@@ -40,16 +48,18 @@ func Solve() {
 		}
 	}
 
+	total := 0
 	for y, row := range grid {
 		for x, _ := range row {
-			res := FloodFill(grid, x, y, grid[y][x])
-			if res > 0 {
-				fmt.Println(res)
+			result := &FloodFillResult{Area: 0, Perimeter: 0}
+			FloodFill(grid, x, y, grid[y][x], result)
+			if result.Area > 0 {
+				fmt.Println(result.Area, result.Perimeter)
+				total += result.Area * result.Perimeter
 			}
 		}
 	}
 
-	// fmt.Println("\nDay 12")
-	// utils.PrintGrid(grid)
-	// fmt.Println("Part 1:")
+	fmt.Println("\nDay 12")
+	fmt.Println("Part 1:", total)
 }
